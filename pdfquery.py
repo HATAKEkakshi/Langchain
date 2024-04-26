76aab7ba310ff09b5c48a4c9a06dec28bf68a507
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-04-26 22:50:34 +0530

    Pdf query

--- pdfquery.py
+++ pdfquery.py
@@ -1,8 +1,10 @@
 import os
 from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import faiss
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAI
 from constants import openai_key
 from typing_extensions import Concatenate
 
@@ -21,4 +23,31 @@ for i ,page in enumerate(pdfreader.pages):
     if content:
         raw_text+= content
 
-print(raw_text)
+
+
+#we need to split the text using character text soilt such that it should not increase token size 
+
+text_spliter=CharacterTextSplitter(
+    separator="\n",
+    chunk_size=800,
+    chunk_overlap=200,
+    length_function =len,
+
+)
+
+texts=text_spliter.split_text(raw_text) 
+len(texts)
+
+#dwonload emeddings from oepnai
+
+embeddings=OpenAIEmbeddings()
+
+document_search =FAISS.from_texts(texts,embeddings)
+
+
+
+chain=load_qa_chain(OpenAI(),chain_type="stuff")
+
+query="what is Rosa"
+docs=document_search.similarity_search(query)
+print(docs[0].page_content)

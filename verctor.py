e566eaceed42706c681816f7ad9eea4823b1d0d0
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-04-28 23:02:35 +0530

    Vectordb

--- verctor.py
+++ verctor.py
@@ -7,8 +7,16 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_community.llms import OpenAI
+from langchain_openai import ChatOpenAI  
+from langchain.chains import RetrievalQA 
+from langchain_community.vectorstores import pinecone
 from dotenv import load_dotenv
 load_dotenv()
+
+llm = ChatOpenAI(   
+    model_name='gpt-3.5-turbo',  
+    temperature=0.7
+) 
 ##lets read the document
 def read_doc(directory):
     file_loader=PyPDFDirectoryLoader(directory)
@@ -28,7 +36,6 @@ documents=chunk_data(docs=doc)
 ##emdding technique of openai
 
 embeddings=OpenAIEmbeddings(api_key=os.environ['OPENAI_API_KEY'])
-print(embeddings)
 
 vector=embeddings.embed_query("what is rosa")
 len(vector)
@@ -43,3 +50,24 @@ index = PineconeVectorStore.from_documents(
         index_name=index_name,
         embedding=embeddings
     )
+#cosine similarity retreive results
+def retrieve_query(query,k=2):
+    matching_results=index.similarity_search(query,k=k)
+    return matching_results
+qa = RetrievalQA.from_chain_type(  
+    llm=llm,  
+    chain_type="stuff",  
+    retriever=index.as_retriever()
+) 
+#search answer from vector database
+"""def retireve_answer(query):
+    doc_search=retrieve_query(query)
+    print(doc_search)
+    response=chain.run(input_documents=doc_search,question=query)
+    return response """
+
+our_query=input("Ask your query")
+"""answer=retireve_answer(our_query)
+print(answer)"""
+response1= qa.run(our_query)
+print(response1)
